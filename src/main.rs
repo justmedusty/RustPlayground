8636397f0d39c9433cdac9ef3762b1de7b4a5494
Dustyn <dustyn_gibb@hotmail.com>
2024-03-03 12:59:55 -0500

    grep clone, only works within project files as is

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use clap::{parser, Parser};
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No pattern given");
-    let path = std::env::args().nth(2).expect("No path given");
-
     let args = Cli::parse();
-    println!("pattern : {:?} , path {:?}",args.pattern,args.path);
+    let content = std::fs::read_to_string(&args.path).expect("Could not read file");
+
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            println!("Found a match! {}",line);
+        }
+    }
 }
 
 #[derive(Parser)]
